af45042ead5a0058b879bf380d2c7fcb5c003f7d
Christian Caunei <christiansilviu.caunei@mail.utoronto.ca>
2026-07-01 20:45:06 -0400

    Nav component, links to all the pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Projects from "./pages/Projects"
+import Nav from "./components/Nav"
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-    </Routes>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </div>
   )
 }
 
